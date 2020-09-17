241295595b7b28f6768774e0c3a16fc07678ee0d
Gautham <gautham18113@gmail.com>
2020-09-17 05:15:37 -0700

    Bugfix flatten method

--- 3-ListsAndRecursion/ListsAndRecursion-6.exs
+++ 3-ListsAndRecursion/ListsAndRecursion-6.exs
@@ -1,11 +1,11 @@
 defmodule Mylist do
   def flatten(list), do: do_flat(list, [])
 
-  defp do_flat([], flat), do: flat
   defp do_flat([h | t], flat) when is_list(h) do
-    do_flat(h, flat)
+    do_flat(h, do_flat(t, flat))
   end
   defp do_flat([h|t], flat) do
-    do_flat(t, flat ++ [h])
+    [h | do_flat(t, flat)]
   end
+  defp do_flat([], flat), do: flat
 end
