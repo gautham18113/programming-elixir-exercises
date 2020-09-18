f28f9a931940c7de80533f89c30949a6647b7251
Gautham Rajendiran <gautham18113@gmail.com>
2020-09-18 08:42:58 -0700

    Fix typo

--- 4-StringsAndBinaries/StringsAndBinaries-3.exs
+++ 4-StringsAndBinaries/StringsAndBinaries-3.exs
@@ -1,6 +1,6 @@
 #
-# Because the head element is interpreted as a liar
-# containing ascii expressions and the tail is different.
+# Because the head element is interpreted as a list
+# containing ascii expressions and the tail is interpreted as a different kind of element.
 #
 # This will work as expected ['cat' | ['dog' | []]]
 #
